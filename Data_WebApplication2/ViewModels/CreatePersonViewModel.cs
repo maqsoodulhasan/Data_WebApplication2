14a920267abc3ea8cfebf0c4658ef44df882458e
maqsoodulhasan <alhasanmps@yahoo.com>
2022-02-07 08:28:36 +0100

    Add project files.

---
+++ Data_WebApplication2/ViewModels/CreatePersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data_WebApplication2.ViewModels
+{
+    public class CreatePersonViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [MinLength(5)]
+        public string PhoneNumber { get; set; }
+        [MaxLength(10)]
+        public string City { get; set; }
+
+    }
+}
